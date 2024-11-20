3a396ac6e4e6f72b0fb09a0d624193934c977551
Lemar Tokham <lemartokham@Lemars-MacBook.local>
2024-11-20 23:19:55 +0000

    Improve solution to valid-parentheses

--- Stack/valid_parentheses.py
+++ Stack/valid_parentheses.py
@@ -1,31 +1,16 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        brackets = {')':'(', ']':'[', '}':'{'}
         stack = []
-        for i in range(len(s)):
-            # pushing open brackets to stack
-            if s[i] == '(' or s[i] == '[' or s[i] == '{':
-                stack.append(s[i])
-            else:
-                # ensuring stack is not empty to avoid stack underflow error
-                if len(stack) == 0:
+
+        # b - bracket
+        for b in s:
+            if b in brackets:
+                # ensure stack is not empty before pop operation
+                if stack and stack[-1] == brackets[b]:
+                    stack.pop()
+                else:
                     return False
-                # compare closed bracket with top of stack, if they are same type, then valid bracket, if not, we return false
-                if s[i] == ')':
-                    if stack[-1] == '(':
-                        stack.pop() 
-                    else:
-                        return False
-                elif s[i] == ']':
-                    if stack[-1] == '[':
-                        stack.pop()
-                    else:
-                        return False
-                elif s[i] == '}':
-                    if stack[-1] == '{':
-                        stack.pop()
-                    else:
-                        return False
-        # if the stack is empty we have removed every bracket, which is only the case when valid pairs are found, so if it is empty, every bracket was valid
+            else:
+                stack.append(b)
         return (len(stack) == 0)
-
-
